d298e736ad120f646bc4cb33202391c4c2782952
bendrendel <bfdrendel@gmail.com>
2021-02-18 21:18:31 -0700

    Finish script, add settings buttons

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEl = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
 const ctx = canvas.getContext('2d');
 
-let size = 20;
+let size = 10;
 let isPressed = false;
 let color = 'black';
 let x;
@@ -26,6 +32,7 @@ canvas.addEventListener('mousemove', e => {
         const x2 = e.offsetX;
         const y2 = e.offsetY;
 
+        drawCircle(x, y);
         drawLine(x, y, x2, y2);
 
         x = x2;
@@ -47,4 +54,29 @@ function drawLine(x1, y1, x2, y2) {
     ctx.strokeStyle = color;
     ctx.lineWidth = size * 2;
     ctx.stroke();
+    console.log(x1, y1, '->', x2, y2);
 }
+
+function updateSizeOnScreen() {
+    sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener('click', () => {
+    size += 5;
+    if (size > 50) {
+        size = 50;
+    }
+    updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener('click', () => {
+    size -= 5;
+    if (size < 5) {
+        size = 5;
+    }
+    updateSizeOnScreen();
+})
+
+colorEl.addEventListener('change', e => color = e.target.value);
+
+clearEl.addEventListener('click', () => ctx.clearRect(0, 0, canvas.width, canvas.height));
